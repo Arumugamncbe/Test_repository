72263e03acc467ab193bf79b1d0fe08aa8237739
Arumugamncbe <166896615+Arumugamncbe@users.noreply.github.com>
2024-04-13 11:52:18 +0530

    Add files via upload

---
+++ MOD5.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+const int height = 100;
+const float number=3.14;
+const char letter = 'A';
+const char letter_seq[10]="BCD";
+printf("height : %d\n", height);
+printf("letter: %c\n", letter);
+printf("letter_seq : %s\n", letter_seq);
+printf("number: %f\n",number);
+getch();
+}
